031e5987037bc6ddb647d5b5da08e3a631b5fd57
Rocky Haque <rockyhaque99@gmail.com>
2025-01-03 22:03:00 +0600

    refactor auth

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -5,11 +5,30 @@ import User from '../module/user/user.model'
 
 const auth = (...requiredRole: string[]) => {
   return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-    const token = req.headers.authorization
-    if (!token) {
+
+    // * Get token from header without Bearer
+    // const token = req.headers.authorization
+    // if (!token) {
+    //   throw new Error('You are not authorized')
+    // }
+
+    // * Get token from header with Bearer
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
       throw new Error('You are not authorized')
     }
 
+    if(!authHeader.startsWith('Bearer')) {
+      throw new Error('Invalid token format. Use Bearer <token>');
+    }
+
+    const token = authHeader.split(' ')[1];
+    // const token = authHeader.slice(7).trim();
+    
+    // console.log(token)
+
+    // Verify the token
     const decoded = jwt.verify(token, "secret") as JwtPayload;
 
     const {email, role} = decoded;
--- src/module/auth/auth.interface.ts
+++ src/module/auth/auth.interface.ts
@@ -1,5 +1,4 @@
 export interface ILoginUser {
-    email: string
-    password: string
-  }
-  
+  email: string
+  password: string
+}
